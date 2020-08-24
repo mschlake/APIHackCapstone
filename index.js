de9af298afa1439421352046b1de191bf58f223d
Matthew Schlake <64107938+mschlake@users.noreply.github.com>
2020-08-24 12:39:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ function displayResults(responseJson) {
   $('#results-list').empty();
   for (let i=0;i<responseJson.results.length;i++){
     $('#results-list').append(
-      `<li class="item">   
-      ${renderImage(responseJson.results[i].image.url,responseJson.results[i].name)}
+      `<li class="item"> 
+      <h3 class>${responseJson.results[i].name}</h3>  
+      <img src="${responseJson.results[i].image.url}" class="results-img">
       <ul>
-        <li>${responseJson.results[i].biography['full-name']} (${responseJson.results[i].appearance.race})</li>
-        <li>Height: ${responseJson.results[i].appearance.height}</li>
-        <li>Weight: ${responseJson.results[i].appearance.weight}</li>
+        <li class="realName">True Identity:<br> ${responseJson.results[i].biography['full-name']} (${responseJson.results[i].appearance.race})</li>
+        <li>Height: <br>${responseJson.results[i].appearance.height}</li>
+        <li>Weight: <br>${responseJson.results[i].appearance.weight}</li>
       </ul>
-      <h4>Power Stats</h4>
+      <h4 class="powerStats">Power Stats</h4>
       <ul>
         <li>Intelligence: ${responseJson.results[i].powerstats.intelligence}</li>
         <li>Strength: ${responseJson.results[i].powerstats.strength}</li>
@@ -47,15 +48,6 @@ function findCharacter(query) {
     .catch(err => {
       $('#js-error-message').text(`Character Not Found. Try using adding a dash ("-")(i.e. Spider-man).`);
     });
-}
-
-// render result images
-function renderImage(url,name){
-  return `<span>
-      <h3 class="toptext" style="
-      background: url(${url});">
-      ${name}</h3>    
-      </span>`
 }
 
 // watch form
